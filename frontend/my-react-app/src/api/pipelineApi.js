f259cdc1a62b42f110fb1348edab637257deab16
Kabeer-developer <kabeeratwork7@gmail.com>
2026-08-30 16:21:40 +0530

    modified for deployment

--- frontend/my-react-app/src/api/pipelineApi.js
+++ frontend/my-react-app/src/api/pipelineApi.js
@@ -1,35 +1,22 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/pipeline",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-
 export const runPipeline = async (domain) => {
-
-  const response = await API.post(
-    "/run",
-    {
-      domain
-    }
-  );
+  const response = await API.post("/run", {
+    domain,
+  });
 
   return response.data.data;
 };
 
-
-export const executePipeline = async (
-  contacts,
-  email
-) => {
-
-  const response = await API.post(
-    "/execute",
-    {
-      contacts,
-      email
-    }
-  );
+export const executePipeline = async (contacts, email) => {
+  const response = await API.post("/execute", {
+    contacts,
+    email,
+  });
 
   return response.data;
 };
